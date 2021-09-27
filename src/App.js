a5b7dce7cb1c1d8ff487458c8ba1f370a51e1225
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2021-09-27 11:16:23 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route, Redirect } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import NewQuote from "./Pages/NewQuote";
-import Quote from "./Pages/Quote";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import QuotesPage from "./Pages/QuotesPage";
-import NotFound from "./Pages/NotFound";
+
+const NewQuote = React.lazy(() => import('./Pages/NewQuote'));
+const Quote = React.lazy(() => import('./Pages/Quote'));
+const NotFound = React.lazy(() => import('./Pages/NotFound'));
 
 function App(props) {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" component={QuotesPage} exact />
-        <Route path="/quotes/:quoteID" component={Quote} />
-        <Route path='/quote/create' component={NewQuote} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Suspense fallback={
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      }>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" component={QuotesPage} exact />
+          <Route path="/quotes/:quoteID" component={Quote} />
+          <Route path='/quote/create' component={NewQuote} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
